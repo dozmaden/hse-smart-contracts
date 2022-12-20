9e78200423ddef11b2c27eabd6961d1b2df225da
dozmaden <ozmadeniz@gmail.com>
2022-12-20 15:35:24 +0300

    Update game, fix bugs

--- contracts/RockPaperScissorsGame.sol
+++ contracts/RockPaperScissorsGame.sol
@@ -3,23 +3,11 @@ pragma solidity ^0.8.4;
 
 contract RockPaperScissorsGame {
 
-    address owner;
-
     enum Shape {
         NULL,
         ROCK,
         PAPER,
         SCISSORS
-    }
-
-    struct Player {
-        address playerAddress;
-        bool isWinner;
-
-        Shape choice;
-        
-        bytes32 hashedShape;
-        uint256 nonce;
     }
 
     enum Status {
@@ -30,61 +18,63 @@ contract RockPaperScissorsGame {
         DONE
     }
 
+    struct Player {
+        address payable playerAddress;
+
+        Shape choice;
+        uint256 nonce;
+        bytes32 hashedShape;
+    }
+
     struct Game {
         Player firstPlayer;
         Player secondPlayer;
-
         Status status;
-        bool isDraw;
+        uint bet;
     }
 
-    mapping(address => Game) public allGames;
+    address owner;
+    uint gameId;
+    mapping(uint => Game) public allGames;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function createGame(Shape choice, uint256 nonce, uint16 validMinutes) external shapeChosen(choice) returns (address) {
-
-        // check if game has already been created
-        require(allGames[msg.sender].firstPlayer.playerAddress == address(0));
-
-        // rock paper scissors game should be valid for 5 minutes
-        require(validMinutes <= 5);
+    function createGame(address payable secondPlayer) external payable returns (uint) { 
+        require(msg.value > 0, "have to bet something!");
 
         Game memory game;
-
-        game.firstPlayer.playerAddress = msg.sender;
-        // hash player choice
-        game.firstPlayer.hashedShape = keccak256(
-            abi.encodePacked(choice, nonce)
-        );
-
-        allGames[msg.sender] = game;
+        game.firstPlayer.playerAddress = payable(msg.sender);
+        game.secondPlayer.playerAddress = payable(secondPlayer);
+        game.bet = msg.value;
         game.status = Status.CREATED;
 
-        return msg.sender;
+        allGames[gameId] = game;
+        return gameId++;
     }
 
-    function playGame(address id,Shape choice, uint256 nonce) external gameExists(id) shapeChosen(choice) {
-        
-        // require that game has been created and the player is distinct
-        require(allGames[id].status < Status.COMMIT_PHASE && msg.sender != allGames[id].firstPlayer.playerAddress);
+    function playGame(uint id, Shape choice, uint nonce) external payable gameInputCorrect(id, choice) {
+        require(allGames[id].status == Status.CREATED);
+        require(msg.value >= allGames[id].bet, "not enough ether sent");
 
-        allGames[id].secondPlayer.playerAddress = msg.sender;
-        allGames[id].secondPlayer.hashedShape = keccak256(abi.encodePacked(choice, nonce));
+        if (msg.sender == allGames[id].firstPlayer.playerAddress) { 
+            allGames[id].firstPlayer.hashedShape = keccak256(abi.encodePacked(choice, nonce));
+        } else if (msg.sender == allGames[id].secondPlayer.playerAddress) { 
+            allGames[id].secondPlayer.hashedShape = keccak256(abi.encodePacked(choice, nonce));
+        }
 
-        allGames[id].status = Status.COMMIT_PHASE;
+        if (msg.value > allGames[id].bet) {
+            payable(msg.sender).transfer(msg.value - allGames[id].bet);
+        }
+
+        if (allGames[id].firstPlayer.hashedShape != 0 && allGames[id].secondPlayer.hashedShape != 0) { 
+            allGames[id].status = Status.COMMIT_PHASE;
+        }
     }
 
-    function revealGame(address id, Shape choice, uint256 nonce) external gameExists(id) shapeChosen(choice) returns (address) {
-
-        // require to be one of the players and still have null shape
-        require((allGames[id].firstPlayer.choice == Shape.NULL && allGames[id].firstPlayer.playerAddress == msg.sender) ||
-                (allGames[id].secondPlayer.choice == Shape.NULL && allGames[id].secondPlayer.playerAddress == msg.sender));
-
-        require(allGames[id].status == Status.COMMIT_PHASE || allGames[id].status == Status.REVEAL_PHASE);
-        allGames[id].status = Status.REVEAL_PHASE;
+    function revealGame(uint id, Shape choice, uint nonce) external gameInputCorrect(id, choice) {
+        require(allGames[id].status == Status.COMMIT_PHASE);
 
         bytes32 hashedShape = keccak256(abi.encodePacked(choice, nonce));
 
@@ -98,39 +88,41 @@ contract RockPaperScissorsGame {
         ) {
             allGames[id].secondPlayer.choice = choice;
             allGames[id].secondPlayer.nonce = nonce;
-        } else { 
-            revert("Cannot confirm you shape choice");
         }
 
-        allGames[id].status = Status.DONE;
-        return chooseWinner(id);
+        if (allGames[id].firstPlayer.choice != Shape.NULL && allGames[id].secondPlayer.choice != Shape.NULL) { 
+            allGames[id].status = Status.REVEAL_PHASE;
+        }
     }
 
-    function chooseWinner(address id) private gameExists(id) returns (address) {
-        require(allGames[id].status == Status.DONE);
+    function chooseWinner(uint id) external returns (address) {
+        require(allGames[id].status == Status.REVEAL_PHASE);
 
-        address first = allGames[id].firstPlayer.playerAddress;
-        address second = allGames[id].secondPlayer.playerAddress;
+        address payable first = allGames[id].firstPlayer.playerAddress;
+        address payable second = allGames[id].secondPlayer.playerAddress;
+        address payable winner;
 
         if (allGames[id].firstPlayer.choice == allGames[id].secondPlayer.choice) {
-            allGames[id].isDraw = true;
-            return address(0);
+            allGames[id].firstPlayer.playerAddress.transfer(allGames[id].bet);
+            allGames[id].secondPlayer.playerAddress.transfer(allGames[id].bet);
+            return winner;
         } else if (allGames[id].firstPlayer.choice == Shape.PAPER) {
-            return allGames[id].secondPlayer.choice == Shape.SCISSORS? second: first;
+            winner = allGames[id].secondPlayer.choice == Shape.SCISSORS? second: first;
         } else if (allGames[id].firstPlayer.choice == Shape.ROCK) {
-            return allGames[id].secondPlayer.choice == Shape.PAPER? second: first;
+            winner = allGames[id].secondPlayer.choice == Shape.PAPER? second: first;
         } else if (allGames[id].firstPlayer.choice == Shape.SCISSORS) {
-            return allGames[id].secondPlayer.choice == Shape.ROCK? second: first;
+            winner = allGames[id].secondPlayer.choice == Shape.ROCK? second: first;
         }
+
+        winner.transfer(allGames[id].bet*2);
+        allGames[id].status = Status.DONE;
+        return winner;
     }
 
-    modifier gameExists(address id) {
+    modifier gameInputCorrect(uint id, Shape shape) {
         require(allGames[id].status != Status.NULL);
-        _;
-    }
-
-    modifier shapeChosen(Shape shape) {
         require(shape != Shape.NULL);
+        require(msg.sender == allGames[id].firstPlayer.playerAddress || msg.sender == allGames[id].secondPlayer.playerAddress);
         _;
     }
 }
